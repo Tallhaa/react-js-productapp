a0a438e604171a5d0ccf93ad0d814d0cc05470aa
Talha <talhajutt4880@gmail.com>
2023-05-26 11:00:12 +0500

    products with api with load more project including load more button first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(8);
+  const [showLoadMore, setShowLoadMore] = useState(false)
+  const getData = async () => {
+    let data = await fetch("https://dummyjson.com/products");
+    data = await data.json();
+    console.log(data.products)
+    setProducts(data.products)
+  }
+  const loadMore = () => {
+    setCount(count + 8)
+
+  }
+  useEffect(() => {
+    getData()
+    setInterval(() => {
+      setShowLoadMore(true)
+    }, 2000);
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Product List</h1>
+      <div style={{ textAlign: 'center' }} className='container'>
+        {
+          products.slice(0, count).map((item) =>
+            <div className='sub-container' key={item.id}>
+              <div className='sub-container-img'>
+                <img className='product-img' src={item.thumbnail} alt="Avatar" />
+              </div>
+              <div class="body-container">
+                <h4>{item.title}</h4>
+                <p>{item.price}</p>
+                <p>{item.id}</p>
+              </div>
+            </div>
+          )
+        }
+      </div>
+      {
+        showLoadMore && (
+          <div className='pagination'>
+            <button className='pagination-btn' onClick={loadMore}>Load More</button>
+          </div>
+        )
+      }
+    </>
+  )
 }
 
 export default App;
